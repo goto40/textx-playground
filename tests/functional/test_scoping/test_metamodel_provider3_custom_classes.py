720e102f1a98423b5737c8ee9e89573808c74c51
goto40 <pierre.bayerl@googlemail.com>
2020-05-01 17:39:16 +0200

    Simplified case: only one metamodel (models of it including models of it)

--- tests/functional/test_scoping/test_metamodel_provider3_custom_classes.py
+++ tests/functional/test_scoping/test_metamodel_provider3_custom_classes.py
@@ -23,6 +23,78 @@ class Obj:
     ref=attr.ib()
 
 
+def test_metamodel_provider_advanced_test3_global_single_metamodel():
+    """
+    simplified test with only one meta model
+    """
+    #################################
+    # META MODEL DEF
+    #################################
+    this_folder = dirname(abspath(__file__))
+
+    def get_meta_model(global_repo, grammar_file_name):
+        mm = metamodel_from_file(join(this_folder, grammar_file_name),
+                                 debug=False, classes=[Cls, Obj])
+        mm.register_scope_providers({
+            "*.*": global_repo,
+        })
+        return mm
+
+    global_repo_provider = scoping_providers.PlainNameGlobalRepo()
+    global_repo_provider.register_models(
+        join(this_folder, "metamodel_provider3", "single", "*.a"))
+
+    a_mm = get_meta_model(
+        global_repo_provider, join(this_folder,
+                                   "metamodel_provider3", "A.tx"))
+
+    clear_language_registrations()
+    register_language(
+        'a-dsl',
+        pattern='*.a',
+        description='Test Lang A',
+        metamodel=a_mm)
+
+    #################################
+    # MODEL PARSING
+    #################################
+
+    model_repo = global_repo_provider.load_models_in_model_repo().all_models
+
+    #################################
+    # TEST MODEL
+    #################################
+
+    def get_all(model_repo, what):
+        lst = []
+        for m in model_repo.filename_to_model.values():
+            lst = lst + get_children_of_type(what, m)
+        return lst
+
+    lst = get_all(model_repo, "Obj")
+    # print(lst)
+    assert len(lst) == 3
+
+    # check some references to be resolved (!=None)
+    for a in lst:
+        assert a.ref
+
+    # check meta classes
+    assert a_mm["Cls"]._tx_fqn == a_mm["Cls"]._tx_fqn
+
+    # more checks
+    from textx import textx_isinstance
+    for a in lst:
+        assert textx_isinstance(a, a_mm["Obj"])
+        assert textx_isinstance(a, a_mm["Obj"])
+        assert textx_isinstance(a, a_mm["Obj"])
+
+    #################################
+    # END
+    #################################
+    clear_language_registrations()
+
+
 def test_metamodel_provider_advanced_test3_global():
     """
     Advanced test for ExtRelativeName and PlainNameGlobalRepo.
